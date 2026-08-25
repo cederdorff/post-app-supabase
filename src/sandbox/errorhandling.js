1a6641175460f9edcf4bd68623601feba5d86788
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2026-08-25 08:26:32 +0200

    refactor: simplify error handling in getPost function

--- src/sandbox/errorhandling.js
+++ src/sandbox/errorhandling.js
@@ -6,24 +6,10 @@ const headers = {
 
 async function getPost(id) {
   const response = await fetch(`${URL}?id=eq.${id}`, { headers });
-
-  if (!response.ok) {
-    throw new Error(`HTTP error: ${response.status}`);
-  }
-
   const data = await response.json();
+  console.log(data);
   const post = data[0];
-
-  if (!post) {
-    throw new Error("Post not found");
-  }
-
   return post;
 }
 
-try {
-  const post = await getPost(5);
-  console.log(post);
-} catch (caughtError) {
-  console.error(caughtError.message);
-}
+// getPost(4);
