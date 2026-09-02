d0f44e02167fa9832b6b0d828bc6347c2adf0c78
RACE - Rasmus Cederdorff <rasmus@cederdorff.com>
2026-09-02 11:03:06 +0200

    perf: lazy-load post detail route

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import { lazy, Suspense } from "react";
 import { BrowserRouter, Routes, Route } from "react-router";
 import Header from "./components/Header";
 import HomePage from "./pages/HomePage";
 import CreatePage from "./pages/CreatePage";
-import PostDetailPage from "./pages/PostDetailPage";
 import UpdatePage from "./pages/UpdatePage";
+
+const PostDetailPage = lazy(() => import("./pages/PostDetailPage"));
 
 function App() {
   return (
@@ -12,7 +14,20 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/create" element={<CreatePage />} />
-        <Route path="/posts/:id" element={<PostDetailPage />} />
+        <Route
+          path="/posts/:id"
+          element={
+            <Suspense
+              fallback={
+                <main className="app">
+                  <p>Loading post...</p>
+                </main>
+              }
+            >
+              <PostDetailPage />
+            </Suspense>
+          }
+        />
         <Route path="/posts/:id/update" element={<UpdatePage />} />
       </Routes>
     </BrowserRouter>
